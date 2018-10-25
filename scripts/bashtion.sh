ecf417ebd0ed5ca934735795e11db707c7d77773
Tim Harrison <tim@tjth.co>
2018-10-25 16:33:57 +0100

    Added ability to set destination server username

--- scripts/bashtion.sh
+++ scripts/bashtion.sh
@@ -17,16 +17,32 @@ groups=$(cat $conf | jq -r ".users[] | select(.username=="\"$(whoami)"\") | .use
 
 # Get servers in Groups
 for group in $groups; do
-cat $conf | jq ".hosts[] | select(.usergroups[]=="\"$group"\") | .name" >> $srvList
+cat $conf | jq -r ".hosts[] | select(.usergroups[]=="\"$group"\") | .name" >> $srvList
 done
 
-# Format server list with numbers prepended
-list=$(nl -w1 -nrz $srvList)
+# Format server list for Dialog
+sed -i -r 's/(\S+)/\1 \1/g' $srvList
+list=$(cat $srvList)
 
 # Generate list
-choice=$(dialog --clear --backtitle "Server Connection" --title "Server List" --menu "Which Server would you like to connect to?" 15 40 4 \
-        $list 2>&1 >/dev/tty)
+choice=$(dialog --clear --backtitle "Server Connection" \
+                --title "Server List" \
+                --menu "Which Server would you like to connect to?" 15 40 6 \
+                $list 2>&1 >/dev/tty)
+# Retreive choice exit code
+choiceVal=$?
+
+# Proceed based on option selected
+case $choiceVal in
+	0)
+	user=$(cat $conf | jq -r ".hosts[] | select(.name=="\"$choice"\") | .sshuser")
+	echo "ssh $user@"$choice"";;
+  1)
+    echo "Cancel pressed.";;
+  255)
+    echo "ESC pressed.";;
+esac
 
 rm -rf /tmp/server-list.txt
 
-exit
+#exit
